f862c6e6a578a5922667c5a5fe22f487373c777a
bjarneij@stud.ntnu.no <bjarneij@stud.ntnu.no>
2020-11-05 22:10:04 +0100

    Added create tables with mysql instead of MS SQL

---
+++ database-scripts/CreateTables mysql.sql
@@ -0,0 +1,67 @@
+--Use script below to drop all the tables
+/************************************************
+
+DROP TABLE Thumbnails;
+
+DROP TABLE Posters;
+
+DROP TABLE Projects;
+
+DROP TABLE ProjectCategories;
+
+DROP TABLE Employers;
+
+************************************************/
+
+CREATE TABLE ProjectCategories
+	(
+		CategoryId int  UNSIGNED AUTO_INCREMENT PRIMARY KEY
+		,CategoryName varchar(50) NOT NULL
+	);
+
+CREATE TABLE Employers
+	(
+		EmployerId int  UNSIGNED AUTO_INCREMENT PRIMARY KEY
+		,EmployerName varchar(50) NOT NULL
+	);
+
+
+CREATE TABLE Projects
+(
+	ProjectId int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
+	,ProjectTitle       varchar(50) NOT NULL
+	,ProjectDescription varchar(500) NOT NULL
+	,ProjectDate date NOT NULL
+	,CategoryId  int UNSIGNED
+	,EmployerId  int UNSIGNED
+	,Active      boolean
+	,Ranking     int UNSIGNED
+	,CONSTRAINT FK_CategoryProject 
+		FOREIGN KEY (CategoryId) 
+		REFERENCES ProjectCategories(CategoryId)
+	,CONSTRAINT FK_EmployerProject 
+		FOREIGN KEY (EmployerId) 
+		REFERENCES Employers(EmployerId)
+);
+
+CREATE TABLE Posters
+(
+	PosterId    int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
+	,ProjectId   int UNSIGNED NOT NULL
+	,PosterDescription varchar(100)
+	,PosterUrl         varchar(100) 
+	,CONSTRAINT FK_PosterProject 
+		FOREIGN KEY(ProjectId) 
+		REFERENCES Projects(ProjectId)
+);
+
+
+CREATE TABLE Thumbnails
+	(
+	ThumbnailId int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
+	,PosterId    int UNSIGNED NOT NULL
+	,PosterUrl   varchar(100) 
+	,CONSTRAINT FK_ThumbnailProject
+		FOREIGN KEY(PosterId) 
+		REFERENCES Posters(PosterId)
+);

8190318884afb293867cba2652d8e6c61a029cf6
Kevin Michael Løver <kevinmlo@stud.ntnu.no>
2020-11-10 22:00:08 +0100

    Updated service.js DTG:102200

--- client/src/service.js
+++ client/src/service.js
@@ -3,103 +3,161 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
+export type ProjectCategories = {
+  categoryid: number,
+  categoryName: string,
+};
+
+class categoriesService {
+  //Get project using Categoryid
+  get(categoryid: number) {
+    return axios
+      .get<ProjectCategories>('/categories/' + categoryid)
+      .then((response) => response.data);
+  }
+  //Get all employers
+  getAll() {
+    return axios.get<ProjectCategories[]>('/categories').then((response) => response.data);
+  }
+}
+
+export type Employers = {
+  employerid: number,
+  employerName: string,
+};
+
+class EmployerService {
+  //Get project using employerid
+  get(employerid: number) {
+    return axios.get<Employers>('/employer/' + employerid).then((response) => response.data);
+  }
+  //Get all employers
+  getAll() {
+    return axios.get<Employers[]>('/employer').then((response) => response.data);
+  }
+}
+
 export type Project = {
   projectid: number,
   title: string,
   description: string,
-  projectDate: number, 
+  projectDate: number,
   categoryId: number,
-  employerid: number, 
+  employerid: number,
   active: boolean,
-  ranking: number, 
+  ranking: number,
 };
 
-class ProjectService{
-    //Get project using projectid
-    get(projectid: number){
-        return axios.get<Project>('/projects/' + projectid).then((response) => response.data);
-    }
-//Get all projects
-    getAll() {
-        return axios.get<Project[]>('/projects').then((response) => response.data)
-    }
+class ProjectService {
+  //Get project using projectid
+  get(projectid: number) {
+    return axios.get<Project>('/projects/' + projectid).then((response) => response.data);
+  }
+  //Get all projects
+  getAll() {
+    return axios.get<Project[]>('/projects').then((response) => response.data);
+  }
 
-//Create new projects, needs a title, categoryID and a active status
-create(title:string, categoryId:number, active:boolean){
-  return axios
-    .post<{}, {projectid:number}>('/projects', {title: title, categoryId: categoryId, active: active})
-    .then((response) => response.data)
-}
- 
-//  Deletes a project based on task id 
- delete(projectid: number) {
-  return axios.delete<Project>('/projects/' + projectid).then((response) => response.data);
+  //Create new projects, needs a title, categoryID and a active status
+  create(title: string, categoryId: number, active: boolean) {
+    return axios
+      .post<{}, { projectid: number }>('/projects', {
+        title: title,
+        categoryId: categoryId,
+        active: active,
+      })
+      .then((response) => response.data);
+  }
+
+  //  Deletes a project based on task id
+  delete(projectid: number) {
+    return axios.delete<Project>('/projects/' + projectid).then((response) => response.data);
+  }
+
+  // Update given project.
+  update(project: Project) {
+    return axios.put<Project, void>('/projects', project).then((response) => response.data);
+  }
 }
 
-// Update given project.
-update(project: Project) {
-  return axios.put<Project, void>('/projects', project).then((response) => response.data);
-}
-}
-
-export type Poster ={
-  posterid: number, 
+export type Poster = {
+  posterid: number,
   projectid: number,
-  description: string, 
+  description: string,
   url: string,
 };
 
-class PosterService{
+class PosterService {
   //Get project using posterid
-  get(posterid: number){
-      return axios.get<Poster>('/posters/' + posterid).then((response) => response.data);
+  get(posterid: number) {
+    return axios.get<Poster>('/posters/' + posterid).then((response) => response.data);
   }
-//Get all posters
+
+  //Get posters using projectid
+  get(projectid: number) {
+    return axios.get<Poster>('/posters/' + projectid).then((response) => response.data);
+  }
+
+  //Get all posters
   getAll() {
-      return axios.get<Poster[]>('/posters').then((response) => response.data)
+    return axios.get<Poster[]>('/posters').then((response) => response.data);
   }
 
-//Create new posters, needs a posterid and has to bed linked to a projectid
-create(posterid:string, projectid:number){
-return axios
-  .post<{}, {posterid:number}>('/posters', {posterid: posterid, projectid: projectid})
-  .then((response) => response.data)
+  //Create new posters, needs a posterid and has to bed linked to a projectid
+  create(posterid: string, projectid: number) {
+    return axios
+      .post<{}, { posterid: number }>('/posters', { posterid: posterid, projectid: projectid })
+      .then((response) => response.data);
+  }
+
+  //  Deletes a poster based on task id
+  delete(posterid: number) {
+    return axios.delete<Project>('/posters/' + posterid).then((response) => response.data);
+  }
+
+  // Update given poster.
+  update(poster: Poster) {
+    return axios.put<Poster, void>('/posters', poster).then((response) => response.data);
+  }
 }
 
-//  Deletes a poster based on task id 
-delete(posterid: number) {
-return axios.delete<Project>('/posters/' + posterid).then((response) => response.data);
-}
-
-// Update given poster.
-update(poster: Poster) {
-return axios.put<Poster, void>('/posters', poster).then((response) => response.data);
-}
-}
-
-export type Thumbnails ={
-  thumbnailid: number, 
-  posterid: number, 
-  url: string, 
+export type Thumbnails = {
+  thumbnailid: number,
+  posterid: number,
+  url: string,
 };
 
-class ThumbnailService{
-//  Deletes a project based on thumbnail id 
-delete(thumbnailid: number) {
-return axios.delete<Project>('/thumbnails/' + thumbnailid).then((response) => response.data);
+class ThumbnailService {
+  //Get project using thumbnailsid
+  get(thumbnailid: number) {
+    return axios.get<Thumbnails>('/thumbnails/' + thumbnailid).then((response) => response.data);
+  }
+  //Get all thumbnails
+  getAll() {
+    return axios.get<Thumbnails[]>('/thumbnails').then((response) => response.data);
+  }
+  //  Deletes a project based on thumbnail id
+  delete(thumbnailid: number) {
+    return axios.delete<Project>('/thumbnails/' + thumbnailid).then((response) => response.data);
+  }
+
+  // Update given thumbnail.
+  update(thumbnails: Thumbnails) {
+    return axios.put<Thumbnails, void>('/thumbnails', thumbnails).then((response) => response.data);
+  }
 }
 
-// Update given thumbnail.
-update(thumbnails: Thumbnails) {
-return axios.put<Thumbnails, void>('/thumbnails', thumbnails).then((response) => response.data);
-}
-}
+const categoriesservice = new categoriesService();
+export { categoriesservice };
+
+const employerservice = new EmployerService();
+export { employerservice };
 
 const projectservice = new ProjectService();
-export {projectservice}
+export { projectservice };
 
 const posterservice = new PosterService();
-export {posterservice}
+export { posterservice };
 
 const thumbnailservice = new ThumbnailService();
-export {thumbnailservice}
+export { thumbnailservice };

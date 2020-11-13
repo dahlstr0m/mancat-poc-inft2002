c2cdd291b642cc835ab8a9ffe2be2c525c92e5e5
bjarneij@stud.ntnu.no <bjarneij@stud.ntnu.no>
2020-11-13 22:37:54 +0100

    Minor formatting edit

--- database-scripts/CreateTables mysql.sql
+++ database-scripts/CreateTables mysql.sql
@@ -15,20 +15,20 @@ DROP TABLE Users;
 
 CREATE TABLE ProjectCategories
 	(
-		categoryId int  UNSIGNED AUTO_INCREMENT PRIMARY KEY
+		categoryId 	  int  UNSIGNED AUTO_INCREMENT PRIMARY KEY
 		,categoryName varchar(100) NOT NULL
 	);
 
 CREATE TABLE Employers
 	(
-		employerId int  UNSIGNED AUTO_INCREMENT PRIMARY KEY
+		employerId 	  int  UNSIGNED AUTO_INCREMENT PRIMARY KEY
 		,employerName varchar(100) NOT NULL
 	);
 
 
 CREATE TABLE Projects
 (
-	projectId int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
+	projectId 	 int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
 	,title       varchar(200) NOT NULL
 	,projectDescription varchar(1000) NOT NULL
 	,projectDate date NOT NULL
@@ -46,11 +46,11 @@ CREATE TABLE Projects
 
 CREATE TABLE Posters
 (
-	posterId    int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
-	,projectId   int UNSIGNED NOT NULL
-	,posterDescription varchar(200)
-	,posterUrl         varchar(200)
-	,thumbnailUrl   varchar(200)
+	posterId    		int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
+	,projectId   		int UNSIGNED NOT NULL
+	,posterDescription  varchar(200)
+	,posterUrl          varchar(200)
+	,thumbnailUrl   	varchar(200)
 	,CONSTRAINT FK_PosterProject 
 		FOREIGN KEY(ProjectId) 
 		REFERENCES Projects(ProjectId)
@@ -60,5 +60,5 @@ CREATE TABLE Users
 (
 	userId    int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
 	,username varchar(100)
-	,password         varchar(100)
+	,password varchar(100)
 );

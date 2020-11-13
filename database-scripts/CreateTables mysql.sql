18dd102b62ee2686668b42687371a0bb8490306e
bjarneij@stud.ntnu.no <bjarneij@stud.ntnu.no>
2020-11-13 22:29:00 +0100

    Updated mysql script, deleted old MS SQL scripts

--- database-scripts/CreateTables mysql.sql
+++ database-scripts/CreateTables mysql.sql
@@ -1,8 +1,6 @@
 --Use script below to drop all the tables
 /************************************************
 
-DROP TABLE Thumbnails;
-
 DROP TABLE Posters;
 
 DROP TABLE Projects;
@@ -11,31 +9,33 @@ DROP TABLE ProjectCategories;
 
 DROP TABLE Employers;
 
+DROP TABLE Users;
+
 ************************************************/
 
 CREATE TABLE ProjectCategories
 	(
-		CategoryId int  UNSIGNED AUTO_INCREMENT PRIMARY KEY
-		,CategoryName varchar(50) NOT NULL
+		categoryId int  UNSIGNED AUTO_INCREMENT PRIMARY KEY
+		,categoryName varchar(100) NOT NULL
 	);
 
 CREATE TABLE Employers
 	(
-		EmployerId int  UNSIGNED AUTO_INCREMENT PRIMARY KEY
-		,EmployerName varchar(50) NOT NULL
+		employerId int  UNSIGNED AUTO_INCREMENT PRIMARY KEY
+		,employerName varchar(100) NOT NULL
 	);
 
 
 CREATE TABLE Projects
 (
-	ProjectId int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
-	,ProjectTitle       varchar(50) NOT NULL
-	,ProjectDescription varchar(500) NOT NULL
-	,ProjectDate date NOT NULL
-	,CategoryId  int UNSIGNED
-	,EmployerId  int UNSIGNED
-	,Active      boolean
-	,Ranking     int UNSIGNED
+	projectId int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
+	,title       varchar(200) NOT NULL
+	,projectDescription varchar(1000) NOT NULL
+	,projectDate date NOT NULL
+	,categoryId  int UNSIGNED
+	,employerId  int UNSIGNED
+	,active      boolean
+	,ranking     int UNSIGNED
 	,CONSTRAINT FK_CategoryProject 
 		FOREIGN KEY (CategoryId) 
 		REFERENCES ProjectCategories(CategoryId)
@@ -46,22 +46,19 @@ CREATE TABLE Projects
 
 CREATE TABLE Posters
 (
-	PosterId    int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
-	,ProjectId   int UNSIGNED NOT NULL
-	,PosterDescription varchar(100)
-	,PosterUrl         varchar(100) 
+	posterId    int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
+	,projectId   int UNSIGNED NOT NULL
+	,posterDescription varchar(200)
+	,posterUrl         varchar(200)
+	,thumbnailUrl   varchar(200)
 	,CONSTRAINT FK_PosterProject 
 		FOREIGN KEY(ProjectId) 
 		REFERENCES Projects(ProjectId)
 );
 
-
-CREATE TABLE Thumbnails
-	(
-	ThumbnailId int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
-	,PosterId    int UNSIGNED NOT NULL
-	,ThumbnailUrl   varchar(100) 
-	,CONSTRAINT FK_ThumbnailProject
-		FOREIGN KEY(PosterId) 
-		REFERENCES Posters(PosterId)
+CREATE TABLE Users
+(
+	userId    int UNSIGNED AUTO_INCREMENT  PRIMARY KEY
+	,username varchar(100)
+	,password         varchar(100)
 );
